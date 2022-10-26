726f533be2b0f59f3864ddbd7577b8661ccb6985
Artem Shalaev <artem.shalaev@gmail.com>
2022-10-26 19:27:36 +0200

    Updates

--- app/src/main/java/com/astep/bookoid/data_classes/MyBook.kt
+++ app/src/main/java/com/astep/bookoid/data_classes/MyBook.kt
@@ -1,5 +1,7 @@
 package com.astep.bookoid.data_classes
 
+// TODO: сюда надо будет добавить ID (long) для ListAdapter +
+//  потому что в базе в будущем будет уникальный айди у элементов
 data class MyBook(
     val imageSource: String,
     val title: String,
